d38c5e55c3bf66d5508908b90c6f7539905ae6db
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-15 04:47:39 +0530

    All href changed to Link and LinkContainer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import {Container} from 'react-bootstrap'
 import HomeScreens from './screens/HomeScreens'
+import ProductScreens from './screens/ProductScreens'
 const App=()=>{
   return (
-    <>
+    <Router>
     <Header></Header>
     <main className="py-3">
       <Container>
-        <HomeScreens></HomeScreens>
+        <Route path='/' component={HomeScreens} exact></Route>
+        <Route path='/products/:id' component={ProductScreens} exact></Route>
       </Container>
     </main>
     <Footer></Footer>
-    </>
+    </Router>
   );
 }
 
---
+++ frontend/src/screens/ProductScreens.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreens = () => {
+    return (
+        <div>
+            Product
+        </div>
+    )
+}
+
+export default ProductScreens
