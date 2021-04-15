4c1df7a33471246be8940d24347fd7aec459fa3c
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-15 17:13:59 +0530

    Error removed

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,19 @@ require('dotenv').config()
 
 const connectDB =require('./db/mongoose')
 const productRoutes=require('./routes/productRoutes')
-const {errorHandler,notFound}=require('./middleware/errorMiddleware')
+const {notFound,errorHandler}=require('./middleware/errorMiddleware')
 
 const app=express()
 connectDB()
 if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
 }
-app.use(notFound)
-app.use(errorHandler)
 
 const PORT=process.env.PORT || 5000
 
 app.use('/api/products',productRoutes)
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold)})
