04a72ef921c98bb8caa459c62683dddb3395bdeb
Abhinav Singh <thakurabhinav17122001@gmail.com>
2021-04-15 13:05:27 +0530

    Data fetched using router from database

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,8 @@ const morgan =require('morgan')
 require('colors')
 require('dotenv').config()
 
-const products=require('./data/products')
 const connectDB =require('./db/mongoose')
+const productRoutes=require('./routes/productRoutes')
 
 const app=express()
 connectDB()
@@ -14,18 +14,6 @@ if(process.env.NODE_ENV==='development'){
 
 const PORT=process.env.PORT || 5000
 
-
-app.get('/',(req,res)=>{
-    res.send('API is running')
-})
-
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-
-app.get('/api/products/:id',(req,res)=>{
-    const product=products.find(p=>p._id===req.params.id)
-    res.json(product)
-})
+app.use('/api/products',productRoutes)
 
 app.listen(PORT,()=>{console.log(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`.yellow.bold)})
